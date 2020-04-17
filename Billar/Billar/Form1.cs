9ab429bd2a2fb223dedecaf79db870401c0fab16
Akrozz <58198893+Akrozz@users.noreply.github.com>
2020-04-17 13:49:39 -0500

    Pito

--- Billar/Billar/Form1.cs
+++ Billar/Billar/Form1.cs
@@ -16,5 +16,10 @@ namespace Billar
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int f = 1;
+        }
     }
 }
