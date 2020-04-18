9c79d3f49442031f5c6b01023b412b3b0e2b309f
PC <PC@DESKTOP-2RT1A8T>
2020-04-18 16:53:16 -0500

    menu 4.00

--- Billar/Billar/MainMenuForm.Designer.cs
+++ Billar/Billar/MainMenuForm.Designer.cs
@@ -181,6 +181,7 @@
             this.button3.Size = new System.Drawing.Size(174, 92);
             this.button3.TabIndex = 9;
             this.button3.Text = "Cliente";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
