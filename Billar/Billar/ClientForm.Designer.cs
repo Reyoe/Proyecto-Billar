c154d6f4c0127bae63ef7345977da7b44c6933f9
ALDO ULISES MORAN SILVA <Aldo.Moran@alumno.udg.mx>
2020-04-21 17:38:46 -0500

    Ningun cambio

--- Billar/Billar/ClientForm.Designer.cs
+++ Billar/Billar/ClientForm.Designer.cs
@@ -34,10 +34,10 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Titulo = new System.Windows.Forms.Label();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.btEliminar = new System.Windows.Forms.Button();
             this.btRecargar = new System.Windows.Forms.Button();
             this.btMenu = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
@@ -45,10 +45,9 @@
             // 
             // btnMostrarCli
             // 
-            this.btnMostrarCli.Location = new System.Drawing.Point(420, 222);
-            this.btnMostrarCli.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMostrarCli.Location = new System.Drawing.Point(315, 180);
             this.btnMostrarCli.Name = "btnMostrarCli";
-            this.btnMostrarCli.Size = new System.Drawing.Size(156, 28);
+            this.btnMostrarCli.Size = new System.Drawing.Size(117, 23);
             this.btnMostrarCli.TabIndex = 26;
             this.btnMostrarCli.Text = "Mostrar";
             this.btnMostrarCli.UseVisualStyleBackColor = true;
@@ -56,10 +55,9 @@
             // 
             // btnModificarCli
             // 
-            this.btnModificarCli.Location = new System.Drawing.Point(615, 222);
-            this.btnModificarCli.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnModificarCli.Location = new System.Drawing.Point(461, 180);
             this.btnModificarCli.Name = "btnModificarCli";
-            this.btnModificarCli.Size = new System.Drawing.Size(156, 28);
+            this.btnModificarCli.Size = new System.Drawing.Size(117, 23);
             this.btnModificarCli.TabIndex = 25;
             this.btnModificarCli.Text = "Modificar";
             this.btnModificarCli.UseVisualStyleBackColor = true;
@@ -67,10 +65,9 @@
             // 
             // btnAgregarCli
             // 
-            this.btnAgregarCli.Location = new System.Drawing.Point(207, 222);
-            this.btnAgregarCli.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAgregarCli.Location = new System.Drawing.Point(155, 180);
             this.btnAgregarCli.Name = "btnAgregarCli";
-            this.btnAgregarCli.Size = new System.Drawing.Size(156, 28);
+            this.btnAgregarCli.Size = new System.Drawing.Size(117, 23);
             this.btnAgregarCli.TabIndex = 24;
             this.btnAgregarCli.Text = "Agregar";
             this.btnAgregarCli.UseVisualStyleBackColor = true;
@@ -81,11 +78,10 @@
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Desktop;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.GridColor = System.Drawing.SystemColors.Desktop;
-            this.dataGridView1.Location = new System.Drawing.Point(207, 300);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(155, 244);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(764, 361);
+            this.dataGridView1.Size = new System.Drawing.Size(573, 293);
             this.dataGridView1.TabIndex = 23;
             // 
             // Titulo
@@ -95,10 +91,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Titulo.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.Titulo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Titulo.Location = new System.Drawing.Point(447, 47);
-            this.Titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Titulo.Location = new System.Drawing.Point(335, 38);
             this.Titulo.Name = "Titulo";
-            this.Titulo.Size = new System.Drawing.Size(268, 86);
+            this.Titulo.Size = new System.Drawing.Size(201, 70);
             this.Titulo.TabIndex = 21;
             this.Titulo.Text = "Cliente";
             this.Titulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -106,39 +101,26 @@
             // pictureBox5
             // 
             this.pictureBox5.BackColor = System.Drawing.Color.LightSlateGray;
-            this.pictureBox5.Location = new System.Drawing.Point(192, 15);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox5.Location = new System.Drawing.Point(144, 12);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(795, 118);
+            this.pictureBox5.Size = new System.Drawing.Size(596, 96);
             this.pictureBox5.TabIndex = 22;
             this.pictureBox5.TabStop = false;
             // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackColor = System.Drawing.SystemColors.Desktop;
-            this.pictureBox4.Location = new System.Drawing.Point(192, 140);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(795, 534);
-            this.pictureBox4.TabIndex = 20;
-            this.pictureBox4.TabStop = false;
-            // 
             // btEliminar
             // 
-            this.btEliminar.Location = new System.Drawing.Point(815, 222);
-            this.btEliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btEliminar.Location = new System.Drawing.Point(611, 180);
             this.btEliminar.Name = "btEliminar";
-            this.btEliminar.Size = new System.Drawing.Size(156, 28);
+            this.btEliminar.Size = new System.Drawing.Size(117, 23);
             this.btEliminar.TabIndex = 27;
             this.btEliminar.Text = "Eliminar";
             this.btEliminar.UseVisualStyleBackColor = true;
             // 
             // btRecargar
             // 
-            this.btRecargar.Location = new System.Drawing.Point(283, 161);
-            this.btRecargar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btRecargar.Location = new System.Drawing.Point(212, 131);
             this.btRecargar.Name = "btRecargar";
-            this.btRecargar.Size = new System.Drawing.Size(127, 30);
+            this.btRecargar.Size = new System.Drawing.Size(95, 24);
             this.btRecargar.TabIndex = 28;
             this.btRecargar.Text = "Recargar";
             this.btRecargar.UseVisualStyleBackColor = true;
@@ -146,20 +128,28 @@
             // 
             // btMenu
             // 
-            this.btMenu.Location = new System.Drawing.Point(615, 161);
-            this.btMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btMenu.Location = new System.Drawing.Point(461, 131);
             this.btMenu.Name = "btMenu";
-            this.btMenu.Size = new System.Drawing.Size(156, 28);
+            this.btMenu.Size = new System.Drawing.Size(117, 23);
             this.btMenu.TabIndex = 29;
             this.btMenu.Text = "Menu Principal";
             this.btMenu.UseVisualStyleBackColor = true;
             // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pictureBox4.Location = new System.Drawing.Point(144, 114);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(596, 434);
+            this.pictureBox4.TabIndex = 20;
+            this.pictureBox4.TabStop = false;
+            // 
             // ClientForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Billar.Properties.Resources.BG1;
-            this.ClientSize = new System.Drawing.Size(1179, 690);
+            this.ClientSize = new System.Drawing.Size(884, 561);
             this.Controls.Add(this.btMenu);
             this.Controls.Add(this.btRecargar);
             this.Controls.Add(this.btEliminar);
@@ -170,7 +160,6 @@
             this.Controls.Add(this.Titulo);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ClientForm";
             this.Text = "ClientForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -188,9 +177,9 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label Titulo;
         private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.Button btEliminar;
         private System.Windows.Forms.Button btRecargar;
         private System.Windows.Forms.Button btMenu;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
